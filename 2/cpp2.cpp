df15e20c74455230bf29ab17b39a8f2918a27680
MohamedMagdy279 <mohamedmagdy49200@gmail.com>
2025-04-21 03:42:31 +0200

    Create cpp2.cpp

---
+++ 2/cpp2.cpp
@@ -0,0 +1,42 @@
+
+int main()
+{
+	//first program
+	cout << "***************************\n";
+	cout << "Name:Mohamed abu-Hodhoud.\n";
+	cout << "Age: 44 Years.\n";
+	cout << "Country: Amman.\n";
+	cout << "***************************\n";
+	cout << "\n\n\n";
+
+	//second program
+
+	
+	cout << "************\n";
+	cout << "************\n";
+	cout << "************\n";
+	cout << "************\n";
+
+	cout << "\n\n\n";
+
+	//third program
+	cout << "I Love Programming!\n\n";
+	cout << "I promise to be the best developer ever!\n\n";
+	cout << "I know it will take some time to practice , but I\nwill achieve my goal.\n\n";
+	cout << "Best Regards,\n";
+	cout << "Mohammed Abu-hadhoud.";
+
+	cout << "\n\n\n";
+
+	cout << "*    *\n";
+	cout << "*    *\n";
+	cout << "******\n";
+	cout << "*    *\n";
+	cout << "*    *\n";
+
+
+
+	cout << "\n\n\n\n";
+
+
+}
