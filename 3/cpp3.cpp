215cc29f336fc5b994a5c9a574710e60374e1d2e
MohamedMagdy279 <mohamedmagdy49200@gmail.com>
2025-04-21 04:37:11 +0200

    Create cpp3.cpp

---
+++ 3/cpp3.cpp
@@ -0,0 +1,25 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	//ring a bell
+	cout << "\a";
+
+	cout << "\n\n\n";
+
+	//sec assign
+	cout << "Dear Sir\\Madam,\n\n How are you? \n\n My name is\"Mohamed\", nice to meet you.";
+
+	cout << "\n\n\n";
+
+
+	//third assign
+	cout << "Ali\tAhmed\tLina\nFadi\tZain\tMona";
+
+	return 0;
+
+}
