4e2274784787c918aec29f2ddb768d1dde422a7e
MohamedMagdy279 <mohamedmagdy49200@gmail.com>
2025-04-21 05:19:19 +0200

    Create cpp1.cpp

---
+++ mostafasaad-assign/1/cpp1.cpp
@@ -0,0 +1,38 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   //first prog
+	cout << "         *      "<<endl;
+	cout << "       * * *    \n";
+	cout << "     * * * * *  \n";
+	cout << "    * * * * * *  \n";
+
+	cout << "\n\n\n";
+		//sec prog
+
+	cout << "   *  \n";
+	cout << "  ***  \n";
+	cout << " *****  \n";
+	cout << "  ***    \n";
+	cout << "   *      \n";
+
+	cout << "\n\n\n";
+	//third prog
+
+	cout << "         1      " << endl;
+	cout << "       2 3 2    \n";
+	cout << "     3 4 5 4 3  \n";
+	cout << "   4 5 6 7 6 5 4  \n";
+	cout << "  5 6 7 8 9 8 7 6 5 \n";
+
+	cout << "\n\n\n";
+	//fourth prog
+
+	cout << "       1     \n";
+	cout << "     1   1   \n";
+	cout << "   1   2   1  \n";
+	cout << "  1  3    3  1  \n";
+}
