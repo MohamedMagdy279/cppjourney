fd8fc5854e6a628f8bc201ca3d091959ffb855a7
MohamedMagdy279 <mohamedmagdy49200@gmail.com>
2025-04-21 07:44:12 +0200

    Create cpp2.cpp

---
+++ mostafasaad-assign/1/cpp2.cpp
@@ -0,0 +1,121 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{/*
+	int i1, i2;
+
+	cin >> i1 >> i2;
+
+	cout << i1 << " + " << i2 << "=" << i1 + i2 <<endl;
+
+	cout << i1 << " - " << i2 << "=" << i1 - i2 << endl;
+
+	cout << i1 << " / " << i2 << "=" << i1 / i2 << endl;
+
+	cout << i1 << " * " << i2 << "=" << i1 * i2 << endl;*/
+
+	/*cout << "\n\n\n";
+
+	string name;
+	int id;
+	double grade;
+
+	string name2;
+	int id2;
+	double grade2;
+
+
+	cout << " what is the student 1 name:";
+	cin >> name;
+
+
+	cout << "His id:";
+	cin >> id;
+
+
+
+	cout << "His math exam grade: ";
+	cin >> grade;
+
+
+
+	cout << " what is the student 2 name: ";
+	cin >> name2;
+
+
+	cout << "His id: ";
+	cin >> id2;
+
+
+	cout << "His math exam grade:";
+	cin >> grade2;
+
+	cout << "\n\n";
+
+	cout << "Students grades in math\n" << name << "(with id" << id << ") got grade:" << grade << endl;
+	cout << name2 << "(with id " << id << ") got grade: " << grade2 <<endl;
+	cout << "average grade is " << (grade + grade2) / 2.0;*/
+
+	//int even1, even2, even3, even4;
+	//int odd1, odd2, odd3, odd4;
+
+	//cin >> odd1 >> even1;
+	//cin >> odd2 >> even2;
+	//cin >> odd3 >> even3;
+	//cin >> odd4 >> even4;
+
+	//cout << even1 + even2 + even3 + even4 <<" " << odd1 + odd2 + odd3 + odd4;
+
+	/*
+	int i1, i2, temp;
+	cin >> i1 >> i2;
+	cout << i1 << i2;
+	temp = i1;
+	i1 = i2;
+	i2 = temp;
+	cout << i1 << i2;*/
+
+	/*int  i1, i2, i3, temp;
+
+	
+	cin >> i1 >> i2 >> i3;
+	cout << i1 << i2 << i3 <<endl;
+
+	temp = i1;
+	i1 = i2;
+	i2 = i3;
+	i3 = temp;
+
+	cout << i1 << i2 << i3;*/
+	//int a, b;
+
+	//cin >> a >> b;
+
+	//// Let's code the 2 possible results
+	//int equ_is_1 = a * a;
+	//int equ_is_neg_1 = 2 * a + 1;
+
+	//// The trick: we want to make them in 1 equation
+	//// Where if input is: only 1 equation is computed and the second is zer0
+	//// To do so: convert -1 to 0 and 1 to 1
+	//// With simple math, we can convert [-1 1] to [0 1] range
+
+	//// value 1 for (b 1) and value 0 for (b -1)
+	//int is_1 = (b + 1) / 2;
+	//// value 1 for (b -1) and value 0 for (b 1)
+	//int is_neg_1 = 1 - is_1;
+
+	//// Either 1*something + 0*something for b = 1
+	//// Or     0*something + 1*something for b = -1
+	//cout << is_1 * equ_is_1 + is_neg_1 * equ_is_neg_1;
+
+/*
+int n;
+cin >> n;
+int sum = n*(n + 1) / 2;
+cout << sum;
+*/
+
+}
