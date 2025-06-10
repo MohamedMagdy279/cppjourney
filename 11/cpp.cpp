14c6ccb7e90dc814797e3e12a2bb555cf86b49d8
MohamedMagdy279 <mohamedmagdy49200@gmail.com>
2025-06-10 15:03:57 +0300

    Create cpp.cpp

---
+++ 11/cpp.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <iostream>
+#include <String>
+using namespace std;
+
+
+
+int main()
+{
+	string st = "43.22";
+	int n;
+	double d;
+	float f;
+	d = stod(st);
+	f = stof(st);
+	n = stoi(st);
+	int n1 = 20;
+	double n2 = 33.5;
+	string s1 = to_string(n1);
+	string s2 = to_string(n2);
+
+
+}
