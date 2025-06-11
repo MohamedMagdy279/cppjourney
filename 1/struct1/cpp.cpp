4c6c2d1890103fa9a6d035aa0624fee42ca6650b
MohamedMagdy279 <mohamedmagdy49200@gmail.com>
2025-06-12 00:57:47 +0300

    Create cpp.cpp

---
+++ 1/struct1/cpp.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct strInfo
+{
+    string FirstName;
+    string LastName;
+    int Age;
+    string Phone;
+};
+
+void ReadInfo(strInfo &Info)
+{
+    cout << "Please enter your FirstName?\n";
+    cin >> Info.FirstName;
+
+    cout << "Please enter your LastName?\n";
+    cin >> Info.LastName;
+
+    cout << "Please enter your Age?\n";
+    cin >> Info.Age;
+
+    cout << "Please enter your Phone?\n";
+    cin >> Info.Phone;
+}
+
+void PrintInfo(const strInfo &Info)
+{
+    cout << "First Name: " << Info.FirstName << "\n";
+    cout << "Last Name: " << Info.LastName << "\n";
+    cout << "Age: " << Info.Age << "\n";
+    cout << "Phone: " << Info.Phone << "\n";
+}
+
+int main()
+{
+    strInfo Person1Info;
+    ReadInfo(Person1Info);
+    PrintInfo(Person1Info);
+    return 0;
+}
